9ab6ea141f6f200d5182daff99f258afd3b4f5c8
dharu.pambudi <dharujp2@gmail.com>
2023-06-20 10:53:07 +0700

    Add_AccountModel

---
+++ RasManagement/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace RasManagement.Models
+{
+    [Table("RASProject_Account")]
+    public class Account
+    {
+        [Key]
+        public string Account_Id { get; set; }
+        public string Fullname { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set }
+        public string Birthplace { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}
+
