a5b13ab6ba9d1068a14a46f333049095a0625f02
hijriananda.salam <hijriananda.s@gmail.com>
2023-06-20 11:30:21 +0700

    Change_Account-Add field

--- RasManagement/Models/Account.cs
+++ RasManagement/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
 using System.Text.Json.Serialization;
 
 namespace RasManagement.Models
@@ -8,15 +9,26 @@ namespace RasManagement.Models
     public class Account
     {
         [Key]
-        public string Account_Id { get; set; }
+        public int Account_Id { get; set; }
         public string Fullname { get; set; }
         public string Nickname { get; set; }
         public string Email { get; set; } 
         public string Password { get; set }
-        public string Birthplace { get; set; }
+        public string Place_of_birth { get; set; }
+        public DateTimeOffset Date_of_birth { get; set; }
+        public string Religion { get; set; }
+        public Gender Gender { get; set; }
+        public string Marital_status { get; set; }
+        public string Nationality { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
-
+        public string Photo { get; set; }
+        public virtual Role? Role { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
     }
 }
 
