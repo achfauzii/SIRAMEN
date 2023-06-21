83550fd7310fedaf8d0454134121533354bc9023
dharu.pambudi <dharujp2@gmail.com>
2023-06-21 09:46:20 +0700

    CHANGE_Account-try connect relate to database

--- RasManagement/Models/Account.cs
+++ RasManagement/Models/Account.cs
@@ -9,43 +9,11 @@ namespace RasManagement.Models
     public class Account
     {
         [Key]
-        public int Account_Id { get; set; }
-        public string Fullname { get; set; }
-        public string Nickname { get; set; }
-        public string Email { get; set; } 
+        public string Account_Id { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
-        public string Place_of_birth { get; set; }
-        public DateTimeOffset Date_of_birth { get; set; }
-        public string Religion { get; set; }
-        public Gender Gender { get; set; }
-        public Marital_status Marital_status { get; set; }
-        public Nationality Nationality { get; set; }
-        public Hired_status hired_status { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public string Photo { get; set; }
-        public virtual Role? Role { get; set; }
-        
+
     }
-    public enum Gender
-    {
-        Male,
-        Female
-    }
-    public enum Nationality
-    {
-        Wni,
-        Wna
-    }
-    public enum Marital_status
-    {
-        Maried,
-        Unmaried
-    }
-    public enum Hired_status
-    {
-        Bootcamp,
-        Prohire
-    }
+
 }
 
