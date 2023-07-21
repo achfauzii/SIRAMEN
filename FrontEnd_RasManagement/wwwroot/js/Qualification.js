b20dea2f2c3cb8046e5c3b9e6774fcc35012e28b
reza.aurora <rezaaurora17@gmail.com>
2023-07-21 15:53:05 +0700

    add frontend qualification(baru bisa get)

--- FrontEnd_RasManagement/wwwroot/js/Qualification.js
+++ FrontEnd_RasManagement/wwwroot/js/Qualification.js
@@ -1,63 +1,41 @@
-﻿var table = null;
+﻿
 $(document).ready(function () {
-    debugger;
     const decodedtoken = parseJwt(sessionStorage.getItem("Token"));
     const accid = decodedtoken.AccountId;
-    table = $('#Qualification').DataTable({
-        "ajax": {
-            url: "https://localhost:7177/api/Qualification/accountId?accountId=" + accid,
-            type: "GET",
-            "datatype": "json",
-            "dataSrc": "data",
-            /*headers: {
-                "Authorization": "Bearer " + sessionStorage.getItem("tokenJWT")
-            },*/
-            /*success: function (result) {
-                console.log(result)
-            }*/
+
+    $.ajax({
+        url: "https://localhost:7177/api/Qualification/accountId?accountId=" + accid,
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            debugger;
+            if (response.status === 200) {
+                displayQualification(response.data);
+            } else {
+                console.error('Error fetching data from API:', response.message);
+            }
         },
-
-        "columns": [
-            {
-                render: function (data, type, row, meta) {
-                    return meta.row + meta.settings._iDisplayStart + 1 + "."
-                }
-            },
-            { "data": "name" },
-            { "data": "publisher" },
-            { "data": "publicationYear" },
-            { "data": "validUntil" },
-            {
-                // Menambahkan kolom "Action" berisi tombol "Edit" dan "Delete" dengan Bootstrap
-                "data": null,
-                "render": function (data, type, row) {
-                    var modalId = "modal-edit-" + data.certificateId;
-                    var deleteId = "modal-delete-" + data.certificateId;
-                    return '<button class="btn btn-warning " data-placement="left" data-toggle="modal" data-animation="false" title="Edit" onclick="return GetById(' + row.certificateId + ')"><i class="fa fa-edit"></i></button >' + '&nbsp;' +
-                        '<button class="btn btn-danger" data-placement="right" data-toggle="modal" data-animation="false" title="Delete" onclick="return Delete(' + row.certificateId + ')"><i class="fa fa-trash"></i></button >'
-                }
-            }
-        ],
-
-        "order": [[1, "asc"]],
-        //"responsive": true,
-        //Buat ngilangin order kolom No dan Action
-        "columnDefs": [
-            {
-                "targets": [0, 2, 3, 4, 5],
-                "orderable": false
-            }
-        ],
-        //Agar nomor tidak berubah
-        "drawCallback": function (settings) {
-            var api = this.api();
-            var rows = api.rows({ page: 'current' }).nodes();
-            api.column(1, { page: 'current' }).data().each(function (group, i) {
-                $(rows).eq(i).find('td:first').html(i + 1);
-            });
+        error: function (error) {
+            console.error('Error fetching data from API:', error);
         }
-    })
-})
+    });
+});
+
+
+function displayQualification(data) {
+    var frameworks = data.map(item => item.framework).join(', ');
+    $('#framework').text(frameworks);
+    var programmingLanguages = data.map(item => item.programmingLanguage).join(', ');
+    $('#programming').text(programmingLanguages);
+    var databases = data.map(item => item.database).join(', ');
+    $('#databases').text(databases);
+    var tools = data.map(item => item.tools).join(', ');
+    $('#tools').text(tools);
+    var others = data.map(item => item.others).join(', ');
+    $('#others').text(others);
+
+}
+
 
 function parseJwt(token) {
     var base64Url = token.split('.')[1];
@@ -69,154 +47,3 @@ function parseJwt(token) {
     return JSON.parse(jsonPayload);
 }
 
-function ClearScreen() {
-    $('#CertificateId').val('');
-    $('#Name').val('');
-    $('#Publisher').val('');
-    $('#PublicationYear').val('');
-    $('#ValidUntil').val('');
-    $('#Update').hide();
-    $('#Save').show();
-}
-
-function GetById(CertificateId) {
-    //debugger;
-    $.ajax({
-        url: "https://localhost:7177/api/Certificate/" + CertificateId,
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (result) {
-            //debugger;
-            var obj = result.data; //data yg dapet dr id
-            $('#CertificateId').val(obj.certificateId); //ngambil data dr api
-            $('#Name').val(obj.name);
-            $('#Publisher').val(obj.publisher);
-            $('#PublicationYear').val(obj.publicationYear);
-            $('#ValidUntil').val(obj.validUntil);
-            $('#Modal').modal('show');
-            $('#Save').hide();
-            $('#Update').show();
-        },
-        error: function (errormessage) {
-            alert(errormessage.responseText);
-        }
-    })
-}
-
-function Save() {
-    var Certificate = new Object(); //bikin objek baru
-    Certificate.name = $('#Name').val(); //value dari database
-    Certificate.publisher = $('#Publisher').val();
-    Certificate.publicationYear = $('#PublicationYear').val();
-    Certificate.validUntil = $('#ValidUntil').val();
-    const decodedtoken = parseJwt(sessionStorage.getItem("Token"));
-    const accid = decodedtoken.AccountId;
-    Certificate.accountId = accid;
-    $.ajax({
-        type: 'POST',
-        url: 'https://localhost:7177/api/Certificate',
-        data: JSON.stringify(Certificate), //ngirim data ke api
-        contentType: "application/json; charset=utf-8"
-    }).then((result) => {
-        debugger;
-        if (result.status == result.status == 201 || result.status == 204 || result.status == 200) {
-            //alert("Data Berhasil Dimasukkan");
-            Swal.fire({
-                icon: 'success',
-                title: 'Berhasil',
-                text: 'Data berhasil dimasukkan',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            table.ajax.reload();
-        }
-        else {
-            Swal.fire(
-                'Error!',
-                result.message,
-                'error'
-            )
-        }
-    })
-}
-
-function Delete(CertificateId) {
-    debugger;
-    Swal.fire({
-        title: 'Kamu yakin?',
-        text: "Anda tidak akan bisa mengembalikannya jika memilih Ya!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Ya',
-        cancelButtonText: 'Tidak'
-    }).then((result) => {
-        if (result.value) {
-            $.ajax({
-                url: "https://localhost:7177/api/Certificate/" + CertificateId,
-                type: "DELETE",
-                dataType: "json",
-            }).then((result) => {
-                debugger;
-                if (result.status == 200) {
-                    Swal.fire(
-                        'Berhasil',
-                        'Data sudah dihapus.',
-                        'success'
-                    )
-                    table.ajax.reload();
-                }
-                else {
-                    Swal.fire(
-                        'Error!',
-                        result.message,
-                        'error'
-                    )
-                }
-            });
-        }
-    })
-}
-
-function Update() {
-    debugger;
-    var Certificate = new Object();
-    Certificate.certificateId = $('#CertificateId').val();
-    Certificate.name = $('#Name').val();
-    Certificate.publisher = $('#Publisher').val();
-    Certificate.publicationYear = $('#PublicationYear').val();
-    Certificate.validUntil = $('#ValidUntil').val();
-    const decodedtoken = parseJwt(sessionStorage.getItem("Token"));
-    const accid = decodedtoken.AccountId;
-    Certificate.accountId = accid;
-    $.ajax({
-        url: 'https://localhost:7177/api/Certificate',
-        type: 'PUT',
-        data: JSON.stringify(Certificate),
-        contentType: "application/json; charset=utf-8",
-
-    }).then((result) => {
-        debugger;
-        if (result.status == 200) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Berhasil',
-                text: 'Data berhasil diupdate',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            $('#Modal').modal('hide');
-            table.ajax.reload();
-        }
-        else {
-            Swal.fire(
-                'Error!',
-                result.message,
-                'error'
-            )
-            table.ajax.reload();
-        }
-    });
-}
