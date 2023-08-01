1489d920fd7da0fe86f2b4459d77e0aab7a9f2e7
Ojifauzi <achmadfauzi1604@gmail.com>
2023-08-01 14:55:25 +0700

    Revert "push dulu biar gak ilang"
    This reverts commit 7f00b9ed060f2eaaa25e7db083c68766dbba6679.

--- RasManagement/Models/ProjectRasmanagementContext.cs
+++ RasManagement/Models/ProjectRasmanagementContext.cs
@@ -35,7 +35,7 @@ public partial class ProjectRasmanagementContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server = RAS-AURORA; Database = Project_RASManagement; user id = sa; password = 5aPassword; Encrypt = false; TrustServerCertificate=Yes; MultipleActiveResultSets=True;");
+        => optionsBuilder.UseSqlServer("server = RAS-FAUZI; Database = Project_RASManagement; user id = sa; password = 5aPassword; Encrypt = false; TrustServerCertificate=Yes; MultipleActiveResultSets=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -117,8 +117,7 @@ public partial class ProjectRasmanagementContext : DbContext
                 .HasMaxLength(50)
                 .IsUnicode(false);
             entity.Property(e => e.ValidUntil)
-                .HasMaxLength(50)
-                .IsUnicode(false)
+                .HasColumnType("date")
                 .HasColumnName("Valid_until");
 
             entity.HasOne(d => d.Account).WithMany(p => p.Certificates)
