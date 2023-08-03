4863be90ff35b8f333fad2a29b340a32412423a1
Ojifauzi <achmadfauzi1604@gmail.com>
2023-08-03 09:49:32 +0700

    Update view layout

--- FrontEnd_RasManagement/wwwroot/js/Certificate.js
+++ FrontEnd_RasManagement/wwwroot/js/Certificate.js
@@ -117,6 +117,9 @@ function GetById(CertificateId) {
         type: "GET",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + sessionStorage.getItem("Token")
+        },
         success: function (result) {
             //debugger;
             var obj = result.data; //data yg dapet dr id
