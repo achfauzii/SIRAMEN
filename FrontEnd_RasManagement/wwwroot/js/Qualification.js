b6d3e39bee5863ada261e74a7c4b8bdc50780156
reza.aurora <rezaaurora17@gmail.com>
2023-08-23 13:50:07 +0700

    push ssuper admin

--- FrontEnd_RasManagement/wwwroot/js/Qualification.js
+++ FrontEnd_RasManagement/wwwroot/js/Qualification.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             "Authorization": "Bearer " + sessionStorage.getItem("Token")
         },
         success: function (response) {
-            debugger;
+            //debugger;
             if (response.status === 200) {
                 displayQualification(response.data);
             } else {
@@ -26,15 +26,15 @@ $(document).ready(function () {
 
 
 function displayQualification(data) {
-    var frameworks = data.map(item => item.framework).join(', ');
+    var frameworks = data.map(item => item.framework).filter(filterframework => filterframework != null).join(', ');
     $('#framework').text(frameworks);
-    var programmingLanguages = data.map(item => item.programmingLanguage).join(', ');
+    var programmingLanguages = data.map(item => item.programmingLanguage).filter(filterpl => filterpl != null).join(', ');
     $('#programming').text(programmingLanguages);
-    var databases = data.map(item => item.database).join(', ');
+    var databases = data.map(item => item.database).filter(filterdb => filterdb != null).join(', ');
     $('#databases').text(databases);
-    var tools = data.map(item => item.tools).join(', ');
+    var tools = data.map(item => item.tools).filter(filtertools => filtertools != null).join(', ');
     $('#tools').text(tools);
-    var others = data.map(item => item.others).join(', ');
+    var others = data.map(item => item.others).filter(filterother => filterother != null).join(', ');
     $('#others').text(others);
 
 }
@@ -48,5 +48,117 @@ function parseJwt(token) {
     }).join(''));
 
     return JSON.parse(jsonPayload);
+}
+
+function framework() {
+    var isValid = true;
+
+    $('select[required]').each(function () {
+        var input = $(this);
+        if (!input.val()) {
+            input.next('.error-message-framework').show();
+            isValid = false;
+        } else {
+            input.next('.error-message-framework').hide();
+        }
+    });
+
+    if (!isValid) {
+        return;
+    }
+
+    var Qualification = new Object();
+    Qualification.framework = $('#Framework').val();
+    const decodedtoken = parseJwt(sessionStorage.getItem("Token"));
+    const accid = decodedtoken.AccountId;
+    Qualification.AccountId = accid;
+    $.ajax({
+        type: 'POST',
+        url: 'https://localhost:7177/api/Qualification',
+        data: JSON.stringify(Qualification),
+        contentType: "application/json; charset=utf-8",
+        headers: {
+            "Authorization": "Bearer " + sessionStorage.getItem("Token")
+        },
+    }).then((result) => {
+        //debugger;
+        if (result.status == 200) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success...',
+                text: 'Data has been added!',
+                showConfirmButtom: false,
+                timer: 1500
+            })
+            $('#ModalFramework').modal('hide');
+            location.reload();
+        }
+        else {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Data Gagal dimasukkan!',
+                showConfirmButtom: false,
+                timer: 1500
+            })
+            $('#ModalFramework').modal('hide');
+            location.reload();
+        }
+    })
+}
+
+function programmingLanguage() {
+    var isValid = true;
+
+    $('select[required]').each(function () {
+        var input = $(this);
+        if (!input.val()) {
+            input.next('.error-message-p').show();
+            isValid = false;
+        } else {
+            input.next('.error-message-p').hide();
+        }
+    });
+
+    if (!isValid) {
+        return;
+    }
+
+    var Qualification = new Object();
+    Qualification.programmingLanguage = $('#PLanguage').val();
+    const decodedtoken = parseJwt(sessionStorage.getItem("Token"));
+    const accid = decodedtoken.AccountId;
+    Qualification.AccountId = accid;
+    $.ajax({
+        type: 'POST',
+        url: 'https://localhost:7177/api/Qualification',
+        data: JSON.stringify(Qualification),
+        contentType: "application/json; charset=utf-8",
+        headers: {
+            "Authorization": "Bearer " + sessionStorage.getItem("Token")
+        },
+    }).then((result) => {
+        //debugger;
+        if (result.status == 200) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success...',
+                text: 'Data has been added!',
+                showConfirmButtom: false,
+                timer: 1500
+            })
+            $('#ModalPLanguage').modal('hide');
+            location.reload();
+        }
+        else {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Data Gagal dimasukkan!',
+                showConfirmButtom: false,
+                timer: 1500
+            })
+            $('#ModalPLanguage').modal('hide');
+            location.reload();
+        }
+    })
 }
 
---
+++ RasManagement/ViewModel/RoleVM.cs
@@ -0,0 +1,9 @@
+﻿namespace RasManagement.ViewModel
+{
+    public class RoleVM
+    {
+        public string AccountId { get; set; } = null!;
+        public string RoleId { get; set; } = null!;
+
+    }
+}
