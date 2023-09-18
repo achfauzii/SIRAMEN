3b8c7ed166f29e412be752c122a3d6206451691e
reza.aurora <rezaaurora17@gmail.com>
2023-09-18 14:20:35 +0700

    commit model departmen & turnover

---
+++ RasManagement/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RasManagement.Models;
+
+public partial class Department
+{
+    public int DeptId { get; set; }
+
+    public string NamaDept { get; set; } = null!;
+
+    public virtual ICollection<TurnOver> TurnOvers { get; set; } = new List<TurnOver>();
+}
