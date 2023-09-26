7727c0e3104b915280a7a667122596697c9cd3b7
Ojifauzi <achmadfauzi1604@gmail.com>
2023-09-26 11:10:40 +0700

    Update handsontable

--- FrontEnd_RasManagement/wwwroot/js/ShortListCandidate.js
+++ FrontEnd_RasManagement/wwwroot/js/ShortListCandidate.js
@@ -18,37 +18,209 @@
          
             const hot = new Handsontable(container, {
                 data: resource,
-
-                dataSchema: { fullname: null, position: null, skillset: null, education: null, university: null, domisili: null, birthdate: null, level: null, experienceInYear: null, filteringBy: null, workStatus: null, noticePeriode: null, financialIndustry: null, rawCv: null, cvBerca: null, englishLevel: null, currentSalary: null, expectedSalary: null, negotiable: null, intwByRas: null, intwDateByRas: null, intwUser: null, nameOfUser: null, intwDateUser: null, levelRekom: null, notes: null},
-
                 columns: [
+                  
                     {
                         title: 'Full Name',
                         type: 'text',
                         data: 'fullname',
-                    
+                       
                     },
                     {
                         title: 'Position',
                         type: 'text',
-                        data: 'email',
-                   
+                        data: 'position',
+                        filter: true,
+                        filter_type: 'input',
                     },
                     {
-                        title: 'Skillset',
+                        title: 'Skill Set',
+                        type: 'text',
+                        data: 'skillset',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Major',
+                        type: 'text',
+                        data: 'education',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'University',
+                        type: 'text',
+                        data: 'university',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Domicile',
+                        type: 'text',
+                        data: 'domisili',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Age',
+                        type: 'numeric',
+                        data: 'birthdate',
+                        filter: true,
+                        filter_type: 'numeric',
+                    },
+                    {
+                        title: 'Level',
+                        type: 'text',
+                        data: 'level',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Experience (Years)',
+                        type: 'numeric',
+                        data: 'experienceInYear',
+                        filter: true,
+                        filter_type: 'numeric',
+                    },
+                    {
+                        title: 'Filtering By',
                         type: 'dropdown',
-                        data: 'nickname',
-                        source: ['Add New','Close']
-                   
-
-
-             
-                    
-                    }
-                  
-                   
+                        data: 'filteringBy',
+                        source: ['RAS', 'Recruitment'],
+                      
+                    },
+                    {
+                        title: 'Work Status',
+                        type: 'checkbox',
+                        data: 'workStatus',
+                        filter: true,
+                        filter_type: 'checkbox',
+                    },
+                    {
+                        title: 'Notice Period',
+                        type: 'text',
+                        data: 'noticePeriode',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Financial Industry',
+                        type: 'checkbox',
+                        data: 'financialIndustry',
+                        filter: true,
+                        filter_type: 'checkbox',
+                    },
+                    {
+                        title: 'Raw CV',
+                        type: 'text',
+                        data: 'rawCv',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Berca CV',
+                        type: 'text',
+                        data: 'cvBerca',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'English Level',
+                        type: 'text',
+                        data: 'englishLevel',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Current Salary',
+                        type: 'text',
+                        data: 'currentSalary',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Expected Salary',
+                        type: 'text',
+                        data: 'expectedSalary',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Negotiable',
+                        type: 'checkbox',
+                        data: 'negotiable',
+                        filter: true,
+                        filter_type: 'checkbox',
+                    },
+                    {
+                        title: 'Interview by RAS',
+                        type: 'text',
+                        data: 'intwByRas',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Interview Date by RAS',
+                        type: 'date',
+                        data: 'intwDateByRas',
+                        filter: true,
+                        filter_type: 'date',
+                        dateFormat: 'YYYY-MM-DD', // Modify the date format as needed
+                    },
+                    {
+                        title: 'Interview by User',
+                        type: 'text',
+                        data: 'intwUser',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Name of User',
+                        type: 'text',
+                        data: 'nameOfUser',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Interview Date by User',
+                        type: 'date',
+                        data: 'intwDateUser',
+                        filter: true,
+                        filter_type: 'date',
+                        dateFormat: 'YYYY-MM-DD', // Modify the date format as needed
+                    },
+                    {
+                        title: 'Level Recommendation',
+                        type: 'text',
+                        data: 'levelRekom',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Status',
+                        type: 'text',
+                        data: 'status',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Notes',
+                        type: 'text',
+                        data: 'notes',
+                        filter: true,
+                        filter_type: 'input',
+                    },
+                    {
+                        title: 'Last Modified',
+                        type: 'date',
+                        data: 'lastModified',
+                        filter: true,
+                        filter_type: 'date',
+                        dateFormat: 'YYYY-MM-DD', // Modify the date format as needed
+                    },
                 ],
-
+               
+                
                //colWidths: auto,
                 height: 'auto',
                 width: 'auto',
--- RasManagement/Controllers/ShortlistController.cs
+++ RasManagement/Controllers/ShortlistController.cs
@@ -14,5 +14,38 @@ namespace RasManagement.Controllers
         {
             this.shortlistRepository = shortlistRepository;
         }
+       /* [HttpPost("CreateOrUpdateEmployee")]
+        public IActionResult CreateOrUpdateEmployee([FromBody] EmployeeModel employee)
+        {
+            try
+            {
+                if (employee.AccountId == "")
+                {
+                    // Ini adalah operasi penambahan, karena EmployeeId baru.
+                    // Ini adalah operasi penambahan, karena EmployeeId baru.
+                    var account = new Account
+                    {
+                        AccountId = accountRepository.GetAccountId(),
+                        Fullname = employee.FullName,
+                        Email = employee.Email,
+                        Nickname = employee.NickName,
+
+                    };
+                    _context.Accounts.Add(account);
+                }
+                else
+                {
+                    // Ini adalah operasi pembaruan, karena EmployeeId sudah ada.
+                    //_context.Accounts.Update(employee);
+                }
+
+                _context.SaveChanges();
+                return Ok("Data berhasil disimpan");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Gagal menyimpan data: {ex.Message}");
+            }
+        }*/
     }
 }
