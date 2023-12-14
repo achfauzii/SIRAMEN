c6d26646386d748917cb860ceee5bbd754de8f66
reza.aurora <rezaaurora17@gmail.com>
2023-12-14 12:51:31 +0700

    fix backend client

--- RasManagement/Models/ClientName.cs
+++ RasManagement/Models/ClientName.cs
@@ -2,7 +2,7 @@
 {
     public class ClientName
     {
-        public string Id { get; set; } = null!;
+        public int Id { get; set; }
 
         public string? NameOfClient { get; set; }
     }
---
+++ RasManagement/Repository/ClientNameRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace RasManagement.Repository
+{
+    public class ClientNameRepository : GeneralRepository<ProjectRasmanagementContext, ClientName, int>
+    {
+        private readonly ProjectRasmanagementContext context;
+
+        public ClientNameRepository(ProjectRasmanagementContext context) : base(context)
+        {
+            this.context = context;
+        }
+        public async Task<bool> ClientNameIsExist(string clientname)
+        {
+            // Use AnyAsync to check if any department with the given name exists
+            var clientnameExists = await context.ClientNames.AnyAsync(a => a.NameOfClient == clientname);
+
+            return clientnameExists;
+        }
+
+    }
+}
