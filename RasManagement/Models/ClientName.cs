df40ed39e457e9cf0e99556c9097f58e74b64e8b
reza.aurora <rezaaurora17@gmail.com>
2023-12-14 11:41:15 +0700

    nambah table client

---
+++ RasManagement/Models/ClientName.cs
@@ -0,0 +1,9 @@
+﻿namespace RasManagement.Models
+{
+    public class ClientName
+    {
+        public string Id { get; set; } = null!;
+
+        public string? NameOfClient { get; set; }
+    }
+}
