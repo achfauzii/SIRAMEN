2941a24550a0a38e90ce102e787a8026ede117bc
FayyadArsy <fayyadarsynandi2@gmail.com>
2024-01-03 16:48:50 +0700

    fix warna pie chart statistic

--- FrontEnd_RasManagement/wwwroot/js/ResourceReport/Statistic.js
+++ FrontEnd_RasManagement/wwwroot/js/ResourceReport/Statistic.js
@@ -36,8 +36,8 @@ function chart(labelChart,countChart){
               data: countChart,
               backgroundColor: [
                 'rgb(255, 99, 132)',
+                'rgb(255, 205, 86)',
                 'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
               ],
               hoverOffset: 4
             }]
