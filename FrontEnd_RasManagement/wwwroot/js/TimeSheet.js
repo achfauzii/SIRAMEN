e4a1e0280f5baa36debb9c1e6007633b1b9f94ad
fauzi.achmad <achmadfauzi1604@gmail.com>
2024-01-04 13:59:05 +0700

    Update FE Admin Timesheet

--- FrontEnd_RasManagement/wwwroot/js/TimeSheet.js
+++ FrontEnd_RasManagement/wwwroot/js/TimeSheet.js
@@ -34,35 +34,7 @@
 function submitMonth() {
 
 }
-function save() {
-    // Ganti URL_target dengan URL endpoint API yang sesuai
-    const apiUrl = 'URL_target';
 
-    return fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: "Bearer " + sessionStorage.getItem("Token"),
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(result => {
-            console.log('Data saved:', result);
-            // Lakukan sesuatu jika data berhasil disimpan
-            return result; // Mengembalikan hasil jika diperlukan
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-            // Lakukan sesuatu jika ada kesalahan dalam menyimpan data
-            throw error; // Melempar kembali kesalahan jika diperlukan
-        });
-}
 
 function clearScreen() {
     $("#activity").val("");
