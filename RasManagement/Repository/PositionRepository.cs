9d97b9d106ef32af5887176d801f34defa06627f
reza.aurora <rezaaurora17@gmail.com>
2024-01-11 13:16:46 +0700

    backend

---
+++ RasManagement/Repository/PositionRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace RasManagement.Repository
+{
+    public class PositionRepository : GeneralRepository<ProjectRasmanagementContext, Position, int>
+    {
+        private readonly ProjectRasmanagementContext context;
+
+        public PositionRepository(ProjectRasmanagementContext context) : base(context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Position>> GetPositionByClientId(int clientId)
+        {
+            var positionClient = await context.Positions
+                .Where(e => e.ClientId == clientId)
+                .ToListAsync();
+
+            return positionClient;
+        }
+    }
+}
