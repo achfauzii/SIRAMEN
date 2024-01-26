17d27587bdc3c32e36aa93476eebfeb96f3e4a32
Yogi Prasetio <yogiprasetio25@gmail.com>
2024-01-26 10:49:18 +0700

    fixing code on change password and layout

--- FrontEnd_RasManagement/wwwroot/js/ChangePassword.js
+++ FrontEnd_RasManagement/wwwroot/js/ChangePassword.js
@@ -1,12 +1,19 @@
 ﻿var dataEmployee = [];
 var dataEmployeeCV = [];
 
+var roleId = getUserRole();
 var notification = document.getElementById("notification");
 var element = document.getElementById("btnBell");
 var elementAlert = document.getElementById("alertNotif");
+
+if (roleId == 1 || roleId == 3) {
+  $("#btnBell").hide();
+}
+
 $(document).ready(function () {
   fetchContractPlacement();
 });
+
 function clearScreen() {
   $("#accountId").val("");
   $("#currentPassword").val("");
@@ -343,4 +350,16 @@ function hideAllNotification() {
   $("#btnBell").trigger("click");
   $("#btnBell").classList.add("show");
   $("#alertNotif").classList.add("show");
+}
+function getUserRole() {
+  var token = sessionStorage.getItem("Token");
+  if (token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var payload = JSON.parse(atob(base64));
+
+    return parseInt(payload.RoleId, 10);
+  }
+
+  return null;
 }
