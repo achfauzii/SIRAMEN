5a3d309844e21b6d65f3c6ab2b5c6c703590a8e6
fauzi.achmad <achmadfauzi1604@gmail.com>
2024-03-04 11:45:25 +0700

    recap update code

--- FrontEnd_RasManagement/Controllers/TimesheetPdfController.cs
+++ FrontEnd_RasManagement/Controllers/TimesheetPdfController.cs
@@ -39,7 +39,7 @@ namespace FrontEnd_RasManagement.Controllers
                 {
 
                     PdfPTable table = new PdfPTable(6);
-                    float[] columnWidths = { 3.5f, 10f,1.8f,3f, 3f, 5.2f };
+                    float[] columnWidths = { 3.5f, 10f,1.8f,3.2f, 3f, 5f };
                     table.SetWidths(columnWidths);
                     table.WidthPercentage = 100; 
 
@@ -171,8 +171,6 @@ namespace FrontEnd_RasManagement.Controllers
                     int total = entry.WfhCount + entry.WfoCount;
                     document.Add(new Paragraph(" "));
                     document.Add(table); // Tambahkan tabel ke dokumen
-
-                    document.Add(new Paragraph(" "));
                     document.Add(new Paragraph("Timesheet Total : "+total+" days", FontFactory.GetFont(FontFactory.HELVETICA, 10f)));
 
                     // Menambahkan kolom-kolom untuk tanda tangan
