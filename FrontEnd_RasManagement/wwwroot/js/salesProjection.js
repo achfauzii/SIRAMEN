f4b37985327aafc016abf92797a2e89203be6407
bayu pratama <bayu.bhp9828@gmail.com>
2024-05-06 13:50:36 +0700

    sales projection js

--- FrontEnd_RasManagement/wwwroot/js/salesProjection.js
+++ FrontEnd_RasManagement/wwwroot/js/salesProjection.js
@@ -1,3 +1,4 @@
+const formModal = $('#Modal-addSalesProjection form');
 $(document).ready(function () {
     $('.nav-tabs .nav-item').find('a').each(function () {
         $(this).on('click', function (e) {
@@ -8,6 +9,7 @@ $(document).ready(function () {
         });
     })
     $('.nav-tabs .nav-item').find('a')[0].click()
+
 })
 function generateData(id) {
     $('#salesProjectionTable').DataTable().destroy();
@@ -92,27 +94,86 @@ function generateData(id) {
             { data:"notes"},
             { data:"contractPeriode"},
             { data:"rateCard"},
-            { data:"projectStatus"},
-            { data: "requestBy" },
+            { data: "projectStatus" },
+            {
+                data: null,
+                orderable: false,
+                render: function (data, type, row) {
+                    return (
+                            `
+                        <div class="d-flex flex-row">
+                            <a href="#" class="btn  ml-1 btn-sm p-0 text-info"  style="font-size: 14pt" data-bs-toggle="modal" data-placement="left" data-tooltip="tooltip" title="Edit Status" onclick = "return GetById(${row.id})"><i class="far fa-edit"></i></a>
+                        </div>
+                        `
+                    );
+                },
+                //visible: objDataToken.RoleId != 7,
+            },
         ],
-        columnDefs: [
-            
-        ],
-        createdRow: function (row, data, dataIndex) {
-
-        },
-        drawCallback: function (settings) {
-            var api = this.api();
-            var rows = api.rows({ page: "current" }).nodes();
-            var currentPage = api.page.info().page; // Mendapatkan nomor halaman saat ini
-            var startNumber = currentPage * api.page.info().length + 1; // Menghitung nomor awal baris pada halaman saat ini
-
-            api
-                .column(0, { page: "current" })
-                .nodes()
-                .each(function (cell, i) {
-                    cell.innerHTML = startNumber + i; // Mengupdate nomor baris pada setiap halaman
-                });
-        },
     });
+}
+
+async function dataClient(id) {
+    $.ajax({
+        url: 'https://localhost:7177/api/ClientName/'+id,
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            Authorization: "Beare " + sessionStorage.getItem("Token")
+        },
+        success: function (data) {
+            console.log(data)
+            $('#clientId').children('option:not(:disabled)').remove();
+            data.data.forEach(function (option) {
+
+                let optionElement = `<option>`;
+                optionElement.attr('value', option);
+                optionElement.text(option);
+
+            })
+        },
+        error: function (err) {
+            console.error("error fetching data:" + err)
+        }
+    })
+}
+async function GetById(id) {
+    ClearScreen()
+    const url = 'https://localhost:7177/api/SalesProjection/' + id;
+    const fetchingData = await fetch(url, {
+        headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("Token"),
+        }
+    })
+    const response = await fetchingData.json()
+    dataClient(response.data.clientId)
+}
+
+function ClearScreen() {
+    $('#Modal-addSalesProjection').modal('show')
+    formModal.find('input').each(function (e) {
+        $(this).val("");
+    })
+    formModal.find('select').each(function (e) {
+        $(this).val("").trigger("change");
+    })
+}
+
+function Update() {
+    var isValid = true;
+    //$('#id).find('option[value="'+data+'"]').attr('selected',true).trigger('change')
+
+    formModal.find('input[required] ,select[required]').each(function (e) {
+        var input = $(this);
+        if (!input.val()) {
+            input.next(".error-message").show();
+            isValid = false;
+        } else {
+            input.next(".error-message").hide();
+        }
+    });
+
+    if (!isValid) {
+        return;
+    }
 }
