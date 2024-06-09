0df114c1d351daf33b840af217eae89eb266880e
SAT3A <94332097+SAT3A@users.noreply.github.com>
2024-06-09 15:59:09 +0700

    downloadPerEmployee
    1.  New Feature for download timesheet Overtime per Employee
    2. Add Action button for download
    3. Add new function for generate Overtime Timesheet

--- FrontEnd_RasManagement/wwwroot/js/RecapOvertime.js
+++ FrontEnd_RasManagement/wwwroot/js/RecapOvertime.js
@@ -8,6 +8,12 @@ $(document).ready(function () {
         allowClear: false,
 
     });
+
+    $('#downloadBtn').click(function () {
+
+        downloadRecap(accountId, month, name)
+    });
+
     $.ajax({
         url: "https://localhost:7177/api/ClientName/ClientNameWithStatusOnsite",
         method: "GET",
@@ -15,6 +21,7 @@ $(document).ready(function () {
         headers: {
             Authorization: "Bearer " + sessionStorage.getItem("Token"),
         },
+
         success: function (data) {
             // Create a Set to store unique company names
             var placements = data.data;
@@ -32,6 +39,7 @@ $(document).ready(function () {
                     .text(placement.nameOfClient);
                 $("#companySelect").append(option);
             });
+
         },
         error: function (error) {
             console.error("Error fetching data from API:", error);
@@ -51,11 +59,16 @@ $(document).ready(function () {
 });
 
 
+
+
+
 function submitReportTimesheet() {
+
     var companyName = $('#companySelect').val();
     var month = $('#month').val();
     $("#nullInput").hide();
     var report = $('#reportTimesheet');
+
 
     // Check if companyName or month is empty or null
     if (!companyName || !month) {
@@ -67,6 +80,8 @@ function submitReportTimesheet() {
     report.show();
 
     var downloadUrl = "/TimeSheetPdf/GeneratePdf?companyName=" + encodeURIComponent(companyName) + "&month=" + encodeURIComponent(month);
+    //var downloadUrl = "/TimeSheetPdf/GeneratePdfperEmployee?accountId=" + encodeURIComponent(accountId) + encodeURIComponent(companyName) + "&month=" + encodeURIComponent(month);
+
 
     // Update the download button's href attribute with the new URL
     $("#btn-report").attr("href", downloadUrl);
@@ -87,17 +102,7 @@ function submitReportTimesheet() {
                 Authorization: "Bearer " + sessionStorage.getItem("Token"),
             },
             dataSrc: function (json) {
-                // Populate the accountName filter dropdown with unique names
-                var accountNameFilter = document.getElementById("accountNameFilter");
-                console.log(accountNameFilter);
-                var uniqueNames = [...new Set(json.data.map(item => item.accountName))];
-                accountNameFilter.innerHTML = '<option value="">Select Account Name</option>'; // Reset options
-                uniqueNames.forEach(function (name) {
-                    var option = document.createElement("option");
-                    option.value = name;
-                    option.textContent = name;
-                    accountNameFilter.appendChild(option);
-                });
+
                 return json.data;
             }
         },
@@ -117,11 +122,17 @@ function submitReportTimesheet() {
                 width: "7%",
                 render: function (data, type, row) {
                     return (
+
                         '<div class="d-flex flex-row">' +
-                        '<a href="#" class="ml-1 pt-0 text-primary" data-toggle="tooltip" style="font-size: 14pt" data-placement="left" data-tooltip="tooltip" title="Detail" onclick = "detailRecap(\'' +
+                        '<a href="#" class="ml-1 pt-0 text-primary" data-toggle="tooltip" style="font-size: 14pt" data-placement="left" data-tooltip="tooltip" title="Detail" onclick="detailRecap(\'' +
                         row.accountId + '\', \'' + month + '\', \'' + row.accountName +
                         '\')"><i class="fas fa-info-circle"></i></a>' +
-                        "</div>"
+                        '<a href="#" class="ml-1 pt-0 text-primary" data-toggle="tooltip" style="font-size: 14pt"  data-placement="left" data-tooltip="tooltip" title="Download" onclick="downloadRecap(\'' +
+                        row.accountId + '\', \'' + month + '\', \'' + row.accountName +
+                        '\')"><i class="fas fa-download"></i></a>' +
+
+                        '</div>'
+
                     );
                 },
             },
@@ -157,20 +168,11 @@ function submitReportTimesheet() {
             }
         }
     });
-
-    // Event listener for the account name filter dropdown
-    $('#accountNameFilter').on('change', function () {
-        var selectedName = this.value;
-        table.column(1).search(selectedName).draw();
-    });
 }
 
 
-
-
-
 function detailRecap(accountId, month, name) {
-    debugger;
+
     $("#informationAccountModal").modal("show");
     document.getElementById('namaPegawai').textContent = name;
 
@@ -185,7 +187,7 @@ function detailRecap(accountId, month, name) {
     if ($.fn.DataTable.isDataTable("#timeSheetTable")) {
         $("#timeSheetTable").DataTable().destroy();
     }
-    
+
     $.ajax({
         url: "https://localhost:7177/api/TimeSheet/TimeSheetByAccountIdAndMonth?accountId=" +
             accountId + "&month=" + month,
@@ -248,5 +250,96 @@ function detailRecap(accountId, month, name) {
 
 
 
+function downloadRecap(accountId, month, name) {
+    //$("#informationAccountModal").modal("show");
+    document.getElementById('namaPegawai').textContent = name;
+    var companyName = $('#companySelect').val();
+
+    if (name) {
+        name.textContent = name;
+        console.log("Name element found and set:", name); // Debugging line
+    } else {
+        console.error("Name element not found"); // Debugging line
+    }
+
+    document.getElementById("tableTimeSheet").hidden = false;
+    if ($.fn.DataTable.isDataTable("#timeSheetTable")) {
+        $("#timeSheetTable").DataTable().destroy();
+    }
+
+    $.ajax({
+        url: "https://localhost:7177/api/TimeSheet/TimeSheetByAccountIdAndMonth?accountId=" +
+            accountId + "&month=" + month,
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("Token"),
+        },
+        success: function (response) {
+            var data = response.data;
+            console.log(data);
+
+            //// Filter data to only include rows where flag contains "Overtime"
+            //var filteredData = data.filter(function (item) {
+            //    return item.flag.includes("Overtime");
+            //});
+
+            var downloadUrl = "/TimeSheetPdf/GeneratePdfOvertime?accountId=" + encodeURIComponent(accountId) + "&companyName=" + encodeURIComponent(companyName) + "&month=" + encodeURIComponent(month);
 
 
+            // Update the download button's href attribute with the new URL
+            /*$("#downloadBtn").attr("href", downloadUrl);*/
+            // Create a temporary anchor element to trigger the download
+            var tempAnchor = document.createElement('a');
+            tempAnchor.href = downloadUrl;
+
+            // Optional: if you want the link to open in a new tab
+            // tempAnchor.target = '_blank';
+
+            // Trigger the download
+            tempAnchor.click();
+
+
+            // Initialize DataTable with filtered data
+            table = $("#timeSheetTable").DataTable({
+                data: data,
+                responsive: true,
+                columns: [
+                    {
+                        name: "one",
+                        data: "date",
+                        render: function (data, type, row) {
+                            if (type === "display" || type === "filter") {
+                                // Format tanggal dalam format yang diinginkan
+                                return moment(data).format("DD MMMM YYYY");
+                            }
+                            // Untuk tipe data lain, kembalikan data aslinya
+                            return data;
+                        },
+                    },
+                    { data: "activity" },
+                    { name: "two", data: "flag" },
+                    { data: "category" },
+                    { data: "status" },
+                    { data: "knownBy" },
+                ],
+                rowsGroup: ["one:name", "two:name"],
+                order: [[0, "asc"]],
+                // Background color for rows with flag "Holiday" or "Weekend"
+                createdRow: function (row, data, dataIndex) {
+                    if (data.flag === "Holiday" || data.flag === "Weekend") {
+                        $(row).css("background-color", "#fe6675");
+                        $(row).css("font-weight", "bold");
+                        $(row).find(".fa-edit").hide();
+                    }
+                },
+            });
+        },
+        error: function (error) {
+            console.error("Error fetching data from API:", error);
+        }
+    });
+}
+
+// Assuming you have a function that updates the currentAccountId, currentMonth, and currentAccountName variables
